9cce79c738fa41e3986068128c0e79933ba524d9
Rui Zhu <kevinricky121@gmail.com>
2022-05-03 21:47:44 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,107 @@
+import { useState,useEffect } from 'react';
 import './App.css'
 
+const cardImages = [
+    { "src": "/img/helmet-1.png", matched: false },
+    { "src": "/img/potion-1.png", matched: false },
+    { "src": "/img/ring-1.png", matched: false },
+    { "src": "/img/scroll-1.png", matched: false },
+    { "src": "/img/shield-1.png", matched: false },
+    { "src": "/img/sword-1.png", matched: false },
+];
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Magic Match</h1>
-      <button>New Game</button>
-    </div>
-  );
+    const [cards, setCards] = useState([]);
+    const [turns, setTurns] = useState(0);
+    const [choiceOne, setChoiceOne] = useState(null);
+    const [choiceTwo, setChoiceTwo] = useState(null);
+    const [disabled, setDisabled] = useState(false);
+
+    //start a new game, create the card group
+    const shuffleCards = () => {
+        const shuffleCards = [...cardImages, ...cardImages]
+            .sort(() => Math.random() - 0.5)
+            .map((card) => ({ ...card, id: Math.random() }));
+
+        setCards(shuffleCards);
+        setTurns(0);
+        setChoiceOne(null);
+        setChoiceTwo(null);
+    }
+
+    // handle a choice
+    const handleChoice = (card) => {
+        console.log(card)
+        choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    }
+
+    const resetTurn = () => {
+        setChoiceOne(null);
+        setChoiceTwo(null);
+        setTurns(prevTurns => prevTurns + 1);
+        setDisabled(false);
+    }
+
+    //start a new game
+    useEffect(() => {
+        shuffleCards();
+    }, []);
+
+    //when user click on a card, compare 2 selected cards
+    useEffect(() => {
+        if (choiceOne && choiceTwo) {
+            setDisabled(true);
+
+            if (choiceOne.src === choiceTwo.src) {
+                setCards(prevCards => {
+                    return prevCards.map(card => {
+                        if (card.src === choiceOne.src) {
+                            return { ...card, matched: true };
+                        }
+                        else {
+                            return card;
+                        }
+                    })
+                })
+                resetTurn();
+            } else {
+                setTimeout(() => resetTurn(), 1000);
+            }
+        }
+    }, [choiceOne, choiceTwo]);
+
+    return (
+        <div className="App">
+            <h1>Magic Match</h1>
+            <button onClick={shuffleCards}>New Game</button>
+            
+            
+            
+            
+            
+
+            
+            
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+    );
 }
 
 export default App
